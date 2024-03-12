12c036ac9b0d5722427cd2e34f54374648ce8c0b
Gabriel-Masiero <gmmasierogabriel@gmail.com>
2024-03-11 21:56:54 -0300

    Main View Controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,13 +3,47 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
 
 public class MainViewController implements Initializable {
 
+	@FXML
+	private MenuItem menuItemAlunos;
+	
+	@FXML
+	private MenuItem menuItemUsuarios;
+	
+	@FXML
+	private MenuItem menuItemTreinos;
+	
+	@FXML
+	private MenuItem menuItemSobre;
+	
+	@FXML
+	public void onMenuItemAlunosAction() {
+		System.out.println("Alunos");
+	}
+	
+	@FXML
+	public void onMenuItemUsuariosAction() {
+		System.out.println("Usuários");
+	}
+	
+	@FXML
+	public void onMenuItemTreinosAction() {
+		System.out.println("Treinos");
+	}
+	
+	@FXML
+	public void onMenuItemSobreAction() {
+		System.out.println("Sobre");
+	}
+	
 	
 	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
+	public void initialize(URL url, ResourceBundle rb) {
 
 	}
 
